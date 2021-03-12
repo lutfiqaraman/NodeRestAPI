e1dc75b596663e9cddfed1b4dc7a0472bcae4e5f
lutfiqaraman <lutfiqaraman@gmail.com>
2021-03-12 22:52:14 +0200

    edit connection.js file

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,24 @@
 // Connect MongoDB
-const mongoose = require('mongoose');
-const connection = process.env.DB_CONNECTION;
+require("dotenv").config();
 
-mongoose.connect(connection, { useUnifiedTopology: true, useNewUrlParser: true  }, () => {
-    console.log("Connected to DB");
-});
+const mongoose = require('mongoose');
+
+const username = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+
+const uri = `mongodb+srv://${username}:${password}@cluster0-eqore.mongodb.net/`;
+
+exports.connection = () => {
+    mongoose.connect(
+        uri,
+        {
+            dbName: 'NodePostDB',
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    ).then(() => {
+        console.log('connected to database ...');
+    }).catch((err) => {
+        if (err) throw err;
+    });
+}
