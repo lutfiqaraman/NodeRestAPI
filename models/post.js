0294489ec7bb1c52a3d3edf2a5329f06fba95370
lutfiqaraman <lutfiqaraman@gmail.com>
2021-03-13 10:28:11 +0200

    create a post schema and edit user schema

---
+++ models/post.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+
+    userID: {
+        type: String
+    },
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model('Posts', PostSchema);
