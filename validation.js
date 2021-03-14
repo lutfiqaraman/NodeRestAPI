09d15149be02b186957542d6ecac1fc45b2c58c2
lutfiqaraman <lutfiqaraman@gmail.com>
2021-03-14 22:05:16 +0200

    create validation.js file

---
+++ validation.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const registerValidation = data => {
+    const registrationSchema = Joi.object({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+
+    return registrationSchema.validate(data);
+}
+
+const loginValidation = data => {
+    const loginSchema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required()
+    });
+
+    return loginSchema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
