8d421861303fe1ddcf13c6b107bd8e5e3737c75f
lutfiqaraman <lutfiqaraman@gmail.com>
2021-03-22 20:11:32 +0200

    add delete method to user

--- routes/user.js
+++ routes/user.js
@@ -62,3 +62,26 @@ router.post('/login', async(req, res) => {
     const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
     res.header('auth-token', token).send(token);
 });
+
+// Delete user
+router.delete('/:uid/only', async(req, res) => {
+    try {
+        const removedUser = await User.remove({ _id: req.params.uid });
+        res.json(removedUser);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
+router.delete('/:uid/all', async(req, res) => {
+    try {
+        const removedUser = await User.remove({ _id: req.params.uid, get: req.body });
+        const removedPosts = await Post.deleteMany({ userID: req.params.uid });
+        res.json(removedUser);
+        res.json(removedPosts);
+    } catch(err) {
+        res.json({ message: err });
+    }
+});
+
+module.exports = router;
