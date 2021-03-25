65f4cb2dc12f5be0bedc789bfeb1bb0ecc77c5d6
lutfiqaraman <lutfiqaraman@gmail.com>
2021-03-25 17:33:00 +0200

    restructure app.js

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 require('dotenv/config');
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-//Import Routes
-const postsRoute = require('./routes/posts.routes');
-const userRoute  = require('./routes/user.routes');
+require('./routes/user.routes')(app);
+require('./routes/posts.routes')(app);
 
-app.use('/posts', postsRoute);
-app.use('/user', userRoute);
-
-//Database connection
-const mongodb = require('./db/connection');
-mongodb.connection();
-
-//Routes
-app.get('/', (req, res) => {
-    res.send('We are on HomePage')
-});
-
-
-app.listen(3000);
+module.exports = app;
 
