3babf99f85a341fcb5c07e04c47d60e742537efb
lutfiqaraman <lutfiqaraman@gmail.com>
2021-03-27 10:56:50 +0300

    import .env file

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import { userRoute } from './routes/user.routes.js';
-import { homeRoute } from "./routes/home.routes.js";
+import { homeRoute } from './routes/home.routes.js';
 
 export const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+dotenv.config({
+    path: './config/.env'
+});
+
 userRoute();
 homeRoute();
 
-app.listen(3000, () => {
-    console.log('server is running');
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log('server is running on ' + PORT);
 });
